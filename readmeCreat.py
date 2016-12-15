33ea684955ced4feeb742604119a0adfd72ab8c5
MakeItPossible16 <cjc1005819688@gmail.com>
2016-12-15 16:55:56 +0800

    Site updated: 2016-12-15 16:55:54

---
+++ readmeCreat.py
@@ -0,0 +1,14 @@
+import os
+writeStr = "# [myblog](http://www.makeitpossible.net.cn)\n\n"
+with open("/home/ubuntu/workspace/blog/hexo_blog/blog/source/_posts/README.md",'w') as f:
+    for files in os.listdir("/home/ubuntu/workspace/blog/hexo_blog/blog/source/_posts"):
+            # print files
+            for file in files.split("\n"):
+                if os.path.isfile("/home/ubuntu/workspace/blog/hexo_blog/blog/source/_posts/" + file):
+                    if(file.split(".")[1] == "md"):
+                        if (file.split(".")[0] == "README"):
+                            continue
+                        writeStr =writeStr + "## ["+ file.split('.')[0] +"](" + file +")\n\n"
+    print writeStr
+    f.write(writeStr)
+f.close()
