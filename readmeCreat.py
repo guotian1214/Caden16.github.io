560e8d959f814d86adeace1e25286310a9b6ef6e
MakeItPossible16 <cjc1005819688@gmail.com>
2017-02-14 13:28:16 +0800

    Site updated: 2017-02-14 13:28:14

--- readmeCreat.py
+++ readmeCreat.py
@@ -1,5 +1,6 @@
+# coding:utf8
 import os
-writeStr = "# [myblog](http://www.makeitpossible.net.cn)\n\n"
+writeStr = "---\ntitle: github目录\ndate: 2016-01-01\n---\n## blogMarkdownFile\n## 寻找实习单位,[个人简介](https://makeitpossible16.github.io/about/)\n## [myblog](https://www.makeitpossible16.github.io)\n"
 with open("/home/ubuntu/workspace/blog/hexo_blog/blog/source/_posts/README.md",'w') as f:
     for files in os.listdir("/home/ubuntu/workspace/blog/hexo_blog/blog/source/_posts"):
             # print files
