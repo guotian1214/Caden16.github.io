198d7386d3a0dc910fbb966e68e0246d642ff537
MakeItPossible16 <cjc1005819688@gmail.com>
2017-02-15 12:30:22 +0800

    Site updated: 2017-02-15 12:30:21

--- readmeCreat.py
+++ readmeCreat.py
@@ -2,7 +2,9 @@
 import os
 writeStr = "---\ntitle: github目录\ndate: 2016-01-01\n---\n## blogMarkdownFile\n## 寻找实习单位,[个人简介](https://makeitpossible16.github.io/about/)\n## [myblog](https://www.makeitpossible16.github.io)\n"
 with open("/home/ubuntu/workspace/blog/hexo_blog/blog/source/_posts/README.md",'w') as f:
-    for files in os.listdir("/home/ubuntu/workspace/blog/hexo_blog/blog/source/_posts"):
+    dirFiles = os.listdir("/home/ubuntu/workspace/blog/hexo_blog/blog/source/_posts")
+    dirFiles.sort()
+    for files in dirFiles:
             # print files
             for file in files.split("\n"):
                 if os.path.isfile("/home/ubuntu/workspace/blog/hexo_blog/blog/source/_posts/" + file):
